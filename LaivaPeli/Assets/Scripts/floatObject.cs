2cb394b735b79f86fb52a937538d92e357205c44
Jokuvaanmie <jokuvaanmie@gmail.com>
2018-01-13 23:50:44 +0200

    Scriptausta ja uutta maailmaa
    [*] Uusi isompi maailma
    [*] Veneen kellunta scripti
    [*] Näkymän vaihto E:stä

---
+++ LaivaPeli/Assets/Scripts/floatObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class floatObject : MonoBehaviour {
+    public float waterLevel = 0.0f;
+    public float floatTheshold = 2.0f;
+    public float waterDensity = 0.125f;
+    public float downForce = 1.0f;
+
+
+    float forceFactor;
+    Vector3 floatForce;
+
+
+    // update once per frame
+    void FixedUpdate() {
+        forceFactor = 1.0f - ((transform.position.y - waterLevel) / floatTheshold);
+
+        if (forceFactor > 0.0f)
+        {
+            floatForce = -Physics.gravity * GetComponent<Rigidbody>().mass * (forceFactor - GetComponent<Rigidbody>().velocity.y * waterDensity);
+            floatForce += new Vector3(0.0f, -downForce * GetComponent<Rigidbody>().mass, 0.0f);
+            GetComponent<Rigidbody>().AddForceAtPosition(floatForce, transform.position);
+
+
+        }
+    }
+}
--- LaivaPeli/Assets/Scripts/switchMode.cs
+++ LaivaPeli/Assets/Scripts/switchMode.cs
@@ -9,34 +9,61 @@ public class switchMode : MonoBehaviour {
     public GameObject player;
     public GameObject playerStartPos;
 
+    bool showText;
+    bool isSteering = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        // set to boat
-        if (Input.GetKey("1")){
+
+    void OnTriggerStay(Collider other){
+        showText = true;
+
+        // veneeseen - 3 persoona
+
+        if (Input.GetKeyDown(KeyCode.E)){
+
             boat.GetComponent<Rigidbody>().isKinematic = false;
             boat.GetComponent<vene>().enabled = true;
             boatCamera.SetActive(true);
 
             player.SetActive(false);
+
+            showText = false;
+            isSteering = true;
+
+            Debug.Log("sisään");
+            Debug.Log("Steering: " + isSteering);
+
+
         }
 
+        // takaisin 1 persoona
+
+        if (Input.GetKeyDown(KeyCode.E) && isSteering ){
 
 
-
-        // set to fps
-        if (Input.GetKey("2")){
             boat.GetComponent<Rigidbody>().isKinematic = true;
             boat.GetComponent<vene>().enabled = false;
             boatCamera.SetActive(false);
 
             player.SetActive(true);
             player.transform.position = playerStartPos.transform.position;
+
+            Debug.Log("pois");
+            Debug.Log("Steering: " + isSteering);
+        }
+
+
+    }
+
+
+
+    // teksti 
+    void OnGUI(){
+        if (showText == true){
+            int x = Screen.width / 2;
+            int y = Screen.height / 2;
+
+            GUI.Box(new Rect(x - 10, y, 180, 22), "");
+            GUI.Label(new Rect(x, y, 1000, 1000), "Paina E ajaaksesi veneellä!");
         }
     }
 }
